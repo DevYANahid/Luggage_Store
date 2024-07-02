aab793d40ed65fbec02a95b8eb5dcf2d8d186904
DevYANahid <codealgorithmschool@gmail.com>
2024-07-02 22:21:01 +0600

    login page added

--- resources/js/Components/Frontend_Components/Navbar.jsx
+++ resources/js/Components/Frontend_Components/Navbar.jsx
@@ -1,7 +1,33 @@
-import { Link } from '@inertiajs/react';
-import React from 'react';
 
-const Navbar = () => {
+import React from 'react';
+import { useEffect } from 'react';
+import Checkbox from '@/Components/Checkbox';
+import GuestLayout from '@/Layouts/GuestLayout';
+import InputError from '@/Components/InputError';
+import InputLabel from '@/Components/InputLabel';
+import PrimaryButton from '@/Components/PrimaryButton';
+import TextInput from '@/Components/TextInput';
+import { Head, Link, useForm } from '@inertiajs/react';
+
+const Navbar = ({status, canResetPassword}) => {
+  const { data, setData, post, processing, errors, reset } = useForm({
+    email: '',
+    password: '',
+    remember: false,
+});
+
+useEffect(() => {
+    return () => {
+        reset('password');
+    };
+}, []);
+
+const submit = (e) => {
+    e.preventDefault();
+
+    post(route('login'));
+};
+
   const openNav = () => {
     document.getElementById("mySidebar").style.width = "auto";
   };
@@ -52,47 +78,73 @@ const Navbar = () => {
       </div>
       <div id="mySidebar" className="sidebar">
         <button className="closebtn" onClick={closeNav}>&times;</button>
-        <div className="inner-menu-login not-connected">
-          <div className="inside">
-            <div className="container-home">
-              <div className="hello-menu">Hello,<br /><span className="color-nanny">dear traveler.</span>
-              </div>
-              <div>
-                <div className="social-button-group">
-                  <button className="social-button-login margin-r-15"><i className="social-icon facebook fs-18"></i>Continue with Facebook</button>
-                  <button className="social-button-login"><i className="social-icon google"></i>Continue with Google</button>
-                </div>
-              </div>
-              <div className="d-none d-md-block">
-                <div className="login-or">
-                  Or via email
-                </div>
-                <form name="loginForm" noValidate role="form" autoComplete="off">
-                  <div className="login-form-v2">
-                    <div className="email">
-                      <input placeholder="Email" type="email" name="loginEmail" required className="form-style-v2" />
-                      <div className="text-left" style={{ display: "none" }}> </div>
+        <div className="inner-menu-login not-connected ">
+            <div className="inside">
+                <div className="container-home">
+
+                    <div>
+                        <div className="social-button-group mb-5">
+                            <button className="social-button-login margin-r-15"><i className="social-icon facebook fs-18"></i>Continue with Facebook</button>
+                            <button className="social-button-login"><i className="social-icon google"></i>Continue with Google</button>
+                        </div>
                     </div>
-                    <div className="position-relative">
-                      <input placeholder="Password" autoComplete="off" autoCorrect="off" autoCapitalize="off" spellCheck="false" name="loginPassword" required type="password" className="form-style-v2" />
-                      <div className="input-icon-right-container">
-                        <div className="nanny-icon input-icon-right opened-eye"></div>
-                      </div>
-                      <div className="text-left" style={{ display: "none" }}> </div>
+                    <div className="d-none d-md-block">
+                        <div className="login-or">
+                            Or via email
+                        </div>
+                        {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
+                        <form onSubmit={submit} name="loginForm" noValidate role="form" autoComplete="off">
+                            <div className="login-form-v2">
+
+                                <div className="email mb-3">
+                                    <InputLabel htmlFor="email" value="Email :" />
+                                    <input id="email"
+                                        type="email"
+                                        name="email"
+                                        value={data.email}
+                                        autoComplete="username"
+                                        isFocused={true}
+                                        onChange={(e) => setData('email', e.target.value)} className="form-style-v2" />
+                                    <div className="text-left" style={{ display: "none" }}> </div>
+                                    <InputError message={errors.password} className="mt-2" />
+                                </div>
+                                <div className="position-relative">
+                                    <InputLabel htmlFor="password" value="Password :" />
+                                    <input id="password"
+                                        type="password"
+                                        name="password"
+                                        value={data.password}
+                                        autoComplete="current-password"
+                                        onChange={(e) => setData('password', e.target.value)} className="form-style-v2" />
+                                    <InputError message={errors.password} className="mt-2" />
+                                    <div className="input-icon-right-container">
+                                        <div className="nanny-icon input-icon-right opened-eye"></div>
+                                    </div>
+                                    <div className="text-left" style={{ display: "none" }}> </div>
+                                </div>
+                            </div>
+                            <div className="login-subtitle font-weight-bold color-nanny underline cursor-pointer">
+                                {canResetPassword && (
+                                    <Link href={route('password.request')}> Forgot Password?</Link>
+                                )}
+
+                            </div>
+
+                            <div className="flex items-center justify-end mt-4">
+
+
+                                <PrimaryButton className="nanny-btn-login" disabled={processing}>
+                                    Log in
+                                </PrimaryButton>
+                            </div>
+                        </form>
                     </div>
-                  </div>
-                  <div className="login-subtitle font-weight-bold color-nanny underline cursor-pointer">
-                    Forgot Password?
-                  </div>
-                  <input type="submit" value="Login" className="nanny-btn-login" />
-                </form>
-              </div>
-              <input value="Login" type="button" className="margin-t-10 nanny-btn-login d-md-none" />
-              <div className="login-subtitle">
-                <Link href={route('register')}> No account? <spen className="color-nanny cursor-pointer">Register</spen></Link>
-              </div>
+                    <input value="Login" type="button" className="margin-t-10 nanny-btn-login d-md-none" />
+                    <div className="login-subtitle">
+                        <Link href={route('register')}> No account? <spen className="color-nanny cursor-pointer">Register</spen></Link>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
       </div>
     </div>
